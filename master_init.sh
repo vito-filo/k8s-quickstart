de1d3f2785b05d76ab0b060011221aa8102fe8a1
vito-filo <65964163+vito-filo@users.noreply.github.com>
2022-01-03 20:19:20 +0100

    Update master_init.sh

--- master_init.sh
+++ master_init.sh
@@ -1,38 +1,21 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-for arg in "$@"
-do
-    case $arg in
-        -i | --ip)
-            ip=$2
-            shift
-            shift
-        ;;
-        -h|--help)
-            cat help_master.txt
-            exit 1
-        ;;
-        -*|--*)
-            echo "Unknown argument $1"
-            cat help_master.txt
-            exit 1
-        ;;
-    esac
-done
-if [[ $ip != "" ]]
-then
-    printf "${RED}Step 1/3: install Docker ${NC}\n"
-    sudo apt-get update && sudo apt-get install -qy docker.io
-    printf "${RED}Step 2/3: install Kubernetes tools ${NC}\n"
-    sudo apt-get update && sudo apt-get install -y apt-transport-https
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-    echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list && sudo apt-get update
-    sudo apt-get update && sudo apt-get install -yq kubelet kubeadm kubernetes-cni
-    sudo apt-mark hold kubelet kubeadm kubectl
-    printf "${RED}Step 3/3: Join the cluster ${NC}\n"
-    sudo kubeadm init --apiserver-advertise-address=$ip
-else
-    echo "Invalid parameters";
-    cat help_master.txt
-fi
+# Setup configuration
+printf "${RED}Step 1/3: Setup installation ${NC}\n"
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# Install kubelet kubeadm kubectl docker.io
+printf "${RED}Step 2/3: install k8s tools and Docker ${NC}\n"
+apt-get update
+apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 docker.io
+
+printf "${RED}Step 3/3: Create cluster ${NC}\n"
+kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
