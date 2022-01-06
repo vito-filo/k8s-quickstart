91c1802c7bd49509435dba30bad4462d1645529b
vito-filo <65964163+vito-filo@users.noreply.github.com>
2022-01-06 11:53:23 +0100

    Update master_init.sh

--- master_init.sh
+++ master_init.sh
@@ -2,7 +2,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Setup configuration
-printf "${RED}Step 1/3: Setup installation ${NC}\n"
+printf "${RED}Step 1/4: Setup installation ${NC}\n"
 apt-get update
 apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -11,12 +11,17 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # Install kubelet kubeadm kubectl docker.io
-printf "${RED}Step 2/3: install k8s tools and Docker ${NC}\n"
+printf "${RED}Step 2/4: install k8s tools and Docker ${NC}\n"
 apt-get update
 apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 docker.io
 
-printf "${RED}Step 3/3: Create cluster ${NC}\n"
+printf "${RED}Step 3/4: Create cluster ${NC}\n"
 kubeadm init
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+printf "${RED}Step 4/4: Install Weave Net Add-On ${NC}\n"
+curl -o weave.yaml https://cloud.weave.works/k8s/v1.8/net.yaml
+cat weave.yaml
+kubectl apply -f weave.yaml
